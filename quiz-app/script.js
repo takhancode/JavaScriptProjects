6a7c6507c54241562d60723de75918f71ab95e29
takhancode <talhadua0@email.com>
2026-08-19 16:34:28 +0500

    change

---
+++ quiz-app/script.js
@@ -0,0 +1,80 @@
+
+let currentquestion=0;
+let questionplace=document.querySelector("h2");
+
+let nextbtn=document.getElementById("nextButton");
+let ansbtn=document.getElementsByClassName("ans")
+
+let questions=[
+
+{
+    question:"What Does HTML Stands For?",
+    answers:[
+       "HyperTextMarkupLanguage",
+       "HyperTextPreprocesseor",
+       "HighTextMachineLanguage",
+       "HomeToolMarkupLanguage"
+    ],
+    correct:0
+}
+,
+
+{
+
+    question:"Capital City of Pakistan",
+    answers:[
+        "Lahore",
+        "Islamabad",
+        "Karachi",
+        "Faisalabad"
+    ],
+    correct:1
+
+},
+{
+  question:"Which Html Tag is used to create Hyperlink",
+  answers:[
+    "<link>",
+    "<a>",
+    "<href>",
+    "<url>"
+  ],
+  correct:1
+},
+
+{
+
+question:"who was first governor of Pakistan",
+answers:[
+    "Quaid-e-Azam",
+    "Liaqat ali khan",
+    "Lord Mount",
+    "Rehman Chaudary"
+],
+correct:0
+},
+
+{
+    question:"Which Html Tag is used to insert an image",
+    answers:[
+        "img",
+        "div",
+        "p",
+        "h1"
+    ],
+    correct:0
+}
+
+];
+
+ questionplace.textContent=questions[currentquestion].question;
+
+
+nextbtn.addEventListener("click",function(){
+
+    currentquestion++;
+    questionplace.textContent=questions[currentquestion].question;
+
+   
+})
+
