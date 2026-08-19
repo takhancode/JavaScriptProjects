4db4458e0bcce4b308d91ee7a912516a522d345c
takhancode <talhadua0@email.com>
2026-08-19 17:41:59 +0500

    codeclean

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,97 +1,146 @@
+let currentquestion = 0;
+let score = 0;
+let answered = false;
 
-let currentquestion=0;
-let questionplace=document.querySelector("h2");
+let questionplace = document.querySelector("h2");
+let questionNumber = document.getElementById("questionNumber");
+let nextbtn = document.getElementById("nextButton");
+let ansbtn = document.getElementsByClassName("ans");
+let result = document.getElementById("result");
 
-let nextbtn=document.getElementById("nextButton");
-let ansbtn=document.getElementsByClassName("ans");
+let questions = [
 
-let questions=[
+    {
+        question: "What Does HTML Stand For?",
+        answers: [
+            "HyperText Markup Language",
+            "HyperText Preprocessor",
+            "High Text Machine Language",
+            "Home Tool Markup Language"
+        ],
+        correct: 0
+    },
 
-{
-    question:"What Does HTML Stands For?",
-    answers:[
-       "HyperTextMarkupLanguage",
-       "HyperTextPreprocesseor",
-       "HighTextMachineLanguage",
-       "HomeToolMarkupLanguage"
-    ],
-    correct:0
-}
-,
+    {
+        question: "Capital City of Pakistan?",
+        answers: [
+            "Lahore",
+            "Islamabad",
+            "Karachi",
+            "Faisalabad"
+        ],
+        correct: 1
+    },
 
-{
+    {
+        question: "Which HTML Tag is used to create Hyperlink?",
+        answers: [
+            "<link>",
+            "<a>",
+            "<href>",
+            "<url>"
+        ],
+        correct: 1
+    },
 
-    question:"Capital City of Pakistan",
-    answers:[
-        "Lahore",
-        "Islamabad",
-        "Karachi",
-        "Faisalabad"
-    ],
-    correct:1
+    {
+        question: "Who was the first Governor-General of Pakistan?",
+        answers: [
+            "Quaid-e-Azam",
+            "Liaquat Ali Khan",
+            "Lord Mountbatten",
+            "Rehman Chaudhary"
+        ],
+        correct: 0
+    },
 
-},
-{
-  question:"Which Html Tag is used to create Hyperlink",
-  answers:[
-    "<link>",
-    "<a>",
-    "<href>",
-    "<url>"
-  ],
-  correct:1
-},
-
-{
-
-question:"who was first governor of Pakistan",
-answers:[
-    "Quaid-e-Azam",
-    "Liaqat ali khan",
-    "Lord Mount",
-    "Rehman Chaudary"
-],
-correct:0
-},
-
-{
-    question:"Which Html Tag is used to insert an image",
-    answers:[
-        "img",
-        "div",
-        "p",
-        "h1"
-    ],
-    correct:0
-}
+    {
+        question: "Which HTML Tag is used to insert an image?",
+        answers: [
+            "<img>",
+            "<div>",
+            "<p>",
+            "<h1>"
+        ],
+        correct: 0
+    }
 
 ];
 
- questionplace.textContent=questions[currentquestion].question;
+
+// Display first question
+function showQuestion() {
+
+    questionplace.textContent =
+        questions[currentquestion].question;
+
+    questionNumber.textContent =
+        `Question ${currentquestion + 1}/${questions.length}`;
+
+    ansbtn.forEach((btn, index) => {
+
+        btn.textContent =
+            questions[currentquestion].answers[index];
+
+        btn.disabled = false;
+
+    });
+
+    result.textContent = "";
+
+    answered = false;
+}
 
 
-nextbtn.addEventListener("click",function(){
+// Show first question
+showQuestion();
+
+
+// Answer buttons
+ansbtn.forEach((btn, index) => {
+
+    btn.addEventListener("click", function () {
+
+        if (answered) {
+            return;
+        }
+
+        if (index === questions[currentquestion].correct) {
+
+            result.textContent = "Correct!";
+            score++;
+
+        } else {
+
+            result.textContent = "Wrong!";
+
+        }
+
+        answered = true;
+
+        // Disable all answer buttons
+        ansbtn.forEach((button) => {
+            button.disabled = true;
+        });
+
+    });
+
+});
+
+
+// Next button
+nextbtn.addEventListener("click", function () {
 
     currentquestion++;
-    questionplace.textContent=questions[currentquestion].question;
-    ansbtn.forEach((val,x)=>{
-    val.textContent=questions[currentquestion].answers[x];
-})
-if(currentquestion>=questions.length-1){
-    nextbtn.disabled=true;
-}
-   
-})
 
+    if (currentquestion >= questions.length) {
+        return;
+    }
 
-ansbtn.forEach((btn,index)=>{
-    btn.addEventListener("click",function(){
-        if(index==questions[currentquestion].correct){
-            console.log("correct");
-        }else{
-            console.log("wrong");
-        }
-    })
-})
- 
+    showQuestion();
 
+    if (currentquestion === questions.length - 1) {
+        nextbtn.disabled = true;
+    }
+
+});
