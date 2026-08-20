4374521bfd1144d57bde5d0f21f2821a74b0f76d
takhancode <talhadua0@email.com>
2026-08-20 13:55:55 +0500

    commit

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -5,4 +5,16 @@ let stop=document.getElementById("stop");
 let now = new Date();
 let target = new Date(dateinput.value);
 let difference = target - now;
-let totalDays = difference / 1000 / 60 / 60 / 24;
+let daysValue = Math.floor(difference / 1000 / 60 / 60 / 24);
+let remainingmillisecond = difference - (daysValue * 1000 * 60 * 60 * 24);
+let remaininghours =
+   Math.floor( remainingmillisecond / 1000 / 60 / 60);
+let remainingMillisecondsAfterHours = remainingmillisecond-(remaininghours*60*60*1000);
+let remainingminutes = Math.floor(remainingMillisecondsAfterHours/1000/60);
+let remainingmillisecondafterminutes=remainingMillisecondsAfterHours-(remainingminutes*1000*60);
+let remainingseconds=Math.floor(remainingMillisecondsAfterminutes/1000);
+days.innerText = daysValue;
+hours.innerText = remaininghours;
+min.innerText = remainingminutes;
+sec.innerText = remainingseconds;
+
