1667be8b3f9fbc967e78c93a43a3a5575dd3d0f1
takhancode <talhadua0@gmail.com>
2026-08-21 22:34:18 +0500

    eventlistenerofAdd

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -1,11 +1,13 @@
 let addbtn=document.getElementById("add");
-
-
-
-
-
+let taskinput=document.getElementById("task-input");
+let all=document.getElementById("all");
+let active=document.getElementById("active");
+let completed=document.getElementById("completed");
 
 
 addbtn.addEventListener("click",function(){
-    console.log("clicked");
-})
+console.log(taskinput.value)
+});
+
+
+
