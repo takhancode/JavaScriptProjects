88de91049a3ae376f5d15e793b149213423f0055
takhancode <talhadua0@gmail.com>
2026-08-21 23:49:02 +0500

    codechange

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -1,42 +1,59 @@
-let addbtn=document.getElementById("add");
-let taskinput=document.getElementById("task-input");
-let all=document.getElementById("all");
-let active=document.getElementById("active");
-let completed=document.getElementById("completed");
-let tasks=[];
+let addbtn = document.getElementById("add");
+let taskinput = document.getElementById("task-input");
 
+let tasks = [];
 
-addbtn.addEventListener("click",function(){
-    let taskobj={
-     task:taskinput.value,
-     active:true,
-     completed:false
-    }
+addbtn.addEventListener("click", function(){
+
+    let taskobj = {
+        task: taskinput.value,
+        active: true,
+        completed: false
+    };
+
     tasks.push(taskobj);
-    renderTask();
 
+    renderTask();
 });
 
+
 function renderTask(){
-    tasks.forEach(function(taskobj){
-    let div=document.createElement("div");
-    let checkbox=document.createElement("input");
-    checkbox.type="checkbox";
-    let span=document.createElement("span")
-    span.innerText=taskobj.task;
-    div.appendChild(checkbox);
-    div.appendChild(span);
-    let editbtn = document.createElement("button");
-    editbtn.innerText = "Edit";
-    let deletebtn = document.createElement("button");
-    deletebtn.innerText = "Delete";
-    div.appendChild(editbtn);
-    div.appendChild(deletebtn);
+
     let tasksection = document.querySelector(".task-section");
-    tasksection.appendChild(div);
+
+    tasksection.innerHTML = "";
+
+    tasks.forEach(function(taskobj, index){
+
+        let div = document.createElement("div");
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        let span = document.createElement("span");
+        span.innerText = taskobj.task;
+
+        div.appendChild(checkbox);
+        div.appendChild(span);
+
+        let editbtn = document.createElement("button");
+        editbtn.innerText = "Edit";
+
+        let deletebtn = document.createElement("button");
+        deletebtn.innerText = "Delete";
+
+        deletebtn.addEventListener("click", function(){
+
+            tasks.splice(index, 1);
+
+            renderTask();
+
+        });
+
+        div.appendChild(editbtn);
+        div.appendChild(deletebtn);
+
+        tasksection.appendChild(div);
+
     });
-
-};
-
-
-
+}
