20843e15651c9553943c6d0bbfe739a5c84b9052
takhancode <talhadua0@gmail.com>
2026-08-23 15:56:01 +0500

    codechange

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,28 +1,34 @@
-let cityinput=document.getElementById("city-input");
-let search=document.getElementById("search");
-let error=document.getElementById("error");
-search.addEventListener("click",function(){
-    if(cityinput.value===""){
-        error.style.display="block";
+let cityinput = document.getElementById("city-input");
+let search = document.getElementById("search");
+let error = document.getElementById("error");
+
+search.addEventListener("click", function () {
+
+    if (cityinput.value === "") {
+        error.style.display = "block";
     }
-    else{
+    else {
 
-        error.style.display="none";
-        console.log(cityinput.value);
-       let url = `https://geocoding-api.open-meteo.com/v1/search?name=${cityinput.value}`;
-    fetch(url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(result) {
-        console.log(result);
-    });
+        error.style.display = "none";
+
+        let url = `https://geocoding-api.open-meteo.com/v1/search?name=${cityinput.value}`;
+
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (result) {
+
+                let latitude = result.results[0].latitude;
+                let longitude = result.results[0].longitude;
+
+                let weatherUrl =
+                    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`;
+
+                console.log(weatherUrl);
+
+            });
+
     }
-   
 
-})
-
-
-
-
-
+});
