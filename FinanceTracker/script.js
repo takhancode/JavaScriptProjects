1605d916f79cdb8e7f5763af05554e15844991d4
takhancode <talhadua0@gmail.com>
2026-08-27 13:10:56 +0500

    Add modal open/close logic and form data submission

--- FinanceTracker/script.js
+++ FinanceTracker/script.js
@@ -1,6 +1,11 @@
 const addexpensebutton = document.getElementById("addexpense");
 let expensemodel = document.getElementById("expenseModal");  
+let closebutton=document.getElementById("closebutton");
 
 addexpensebutton.addEventListener("click", function(){
     expensemodel.style.display = "block"; 
 });
+
+closebutton.addEventListener("click",function(){
+    expensemodel.style.display="none";
+})
