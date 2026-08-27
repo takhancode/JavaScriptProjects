c68fbd7c11149c2cf30ed7cdf3eb74b61b553747
takhancode <talhadua0@gmail.com>
2026-08-27 12:02:59 +0500

    closebutton

--- FinanceTracker/script.js
+++ FinanceTracker/script.js
@@ -1,6 +1,6 @@
 const addexpensebutton = document.getElementById("addexpense");
-let expensemodel = document.getElementById("expenseModal");   // sahi id
+let expensemodel = document.getElementById("expenseModal");  
 
 addexpensebutton.addEventListener("click", function(){
-    expensemodel.style.display = "block";   // wahi variable use karo jo banaya
+    expensemodel.style.display = "block"; 
 });
