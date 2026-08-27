2d7d747305605713f44a3d46e00c7ee6fd3c85b3
takhancode <talhadua0@gmail.com>
2026-08-27 21:11:39 +0500

    rendertableadd

--- FinanceTracker/script.js
+++ FinanceTracker/script.js
@@ -1,7 +1,15 @@
 const addexpensebutton = document.getElementById("addexpense");
 let expensemodel = document.getElementById("expenseModal");  
 let closebutton=document.getElementById("closebutton");
-
+let submitbutton=document.getElementById("submit-btn");
+let expense=[];
+let expensename=document.getElementById("expensename");
+let expenseamount=document.getElementById("expenseamount");
+let expensecategory=document.getElementById("expensecategory");
+let expensetype=document.getElementById("expensetype");
+let date=document.getElementById("expensedate");
+let form=document.getElementById("submit-form");
+let tbody=document.getElementById("tablebody")
 addexpensebutton.addEventListener("click", function(){
     expensemodel.style.display = "block"; 
 });
@@ -9,3 +17,26 @@ addexpensebutton.addEventListener("click", function(){
 closebutton.addEventListener("click",function(){
     expensemodel.style.display="none";
 })
+
+submitbutton.addEventListener("click",function(e){
+  e.preventDefault();
+ let name=expensename.value;
+ let amount=expenseamount.value;
+ let category=expensecategory.value;
+ let type=expensetype.value;
+ let expensedate=date.value;
+
+ let newExpense = {
+    expensename: name,
+    expenseamount: parseFloat(amount),
+    expensecategory: category,
+    expensetype: type,
+    date: expensedate
+};
+
+expense.push(newExpense);
+console.log(expense);
+ expensemodel.style.display="none";
+ form.reset();
+})
+
